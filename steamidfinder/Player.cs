2e04337feb7fc9ed152bec8716865627d68f8897
Rafał Załuski <rafal.zaluski@student.put.poznan.pl>
2018-09-13 17:29:47 +0200

    full program

---
+++ steamidfinder/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+public class Infractions
+{
+    public string last_infraction_date { get; set; }
+    public int afk { get; set; }
+    public int leaver { get; set; }
+    public int qm_not_checkedin { get; set; }
+    public int qm_not_voted { get; set; }
+}
+
+public class Platforms
+{
+    public string steam { get; set; }
+}
+
+public class EU
+{
+    public string selected_ladder_id { get; set; }
+}
+
+public class Regions
+{
+    public EU EU { get; set; }
+}
+
+public class Csgo
+{
+    public string game_profile_id { get; set; }
+    public string region { get; set; }
+    public Regions regions { get; set; }
+    public string skill_level_label { get; set; }
+    public string game_player_id { get; set; }
+    public int skill_level { get; set; }
+    public int faceit_elo { get; set; }
+    public string game_player_name { get; set; }
+}
+
+public class Games
+{
+    public Csgo csgo { get; set; }
+}
+
+public class Settings
+{
+    public string language { get; set; }
+}
+
+public class RootObject
+{
+    public string player_id { get; set; }
+    public string nickname { get; set; }
+    public string avatar { get; set; }
+    public string country { get; set; }
+    public string cover_image { get; set; }
+    public string cover_featured_image { get; set; }
+    public Infractions infractions { get; set; }
+    public Platforms platforms { get; set; }
+    public Games games { get; set; }
+    public Settings settings { get; set; }
+    public List<string> friends_ids { get; set; }
+    public List<object> bans { get; set; }
+    public string new_steam_id { get; set; }
+    public string steam_id_64 { get; set; }
+    public string steam_nickname { get; set; }
+    public string membership_type { get; set; }
+    public string faceit_url { get; set; }
+}
---
+++ steamidfinder/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace steamidfinder
+{
+    class Program
+    {
+        public static ConcurrentDictionary<int, string> dictionary = new ConcurrentDictionary<int, string>();
+        public static int count = 0;
+        public static int max;
+        public static string client_id = "";
+        public static string GET(string url)
+        {
+            bool success;
+            int tries = 0;
+            do
+            {
+                success = true;
+                tries++;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Headers.Add("Authorization: Bearer " + client_id);
+                try
+                {
+                    WebResponse response = request.GetResponse();
+                    using (Stream responseStream = response.GetResponseStream())
+                    {
+                        StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                        return reader.ReadToEnd();
+                    }
+                }
+                catch (WebException ex)
+                {
+                    WebResponse errorResponse = ex.Response;
+                    using (Stream responseStream = errorResponse.GetResponseStream())
+                    {
+                        StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                        String errorText = reader.ReadToEnd();
+                        Console.WriteLine(url);
+                        // log errorText
+                    }
+                    success = false;
+                }
+            } while (!success && tries < 5);
+            return null;
+        }
+
+        public static void FindId(string nick, int number)
+        {
+            string html = GET("https://open.faceit.com/data/v4/players?nickname=" + nick);
+            if(html != null)
+            {
+                RootObject player = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(html);
+                dictionary.TryAdd(number, "https://steamcommunity.com/profiles/" + player.steam_id_64);
+            }
+            else
+            {
+                dictionary.TryAdd(number, "-");
+            }
+            Interlocked.Increment(ref count);
+            Console.Clear();
+            Console.WriteLine(count + "/" + Program.max);
+            Console.WriteLine(nick);
+        }
+        static void Main(string[] args)
+        {
+            string inputFilename = @"input.txt";
+            string outputFilename = @"output.txt";
+            File.Delete(outputFilename);
+            if(!File.Exists(outputFilename))
+            {
+                using (StreamWriter sw = File.CreateText(outputFilename))
+                {
+                    string[] lines = File.ReadAllLines(inputFilename);
+                    Program.max = lines.Length;
+                    List<Task> tasks = new List<Task>();
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        var j = i;
+                        tasks.Add(Task.Factory.StartNew(() =>
+                        {
+                            FindId(lines[j], j);
+                        }));
+                    }
+                    Task.WaitAll(tasks.ToArray());
+                    for(int i = 0; i < lines.Length; i++)
+                    {
+                        sw.WriteLine(dictionary[i]);
+                    }
+                    sw.Close();
+                }
+            }
+        }
+    }
+}
